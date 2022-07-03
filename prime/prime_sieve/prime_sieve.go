dcccebe8ada9a14b360bf8afa92ea1b7e39ee540
dkxiaohei <dkxiaohei@outlook.com>
2022-07-03 22:44:10 +0800

    Implement prime sieve in Go

---
+++ prime/prime_sieve/prime_sieve.go
@@ -0,0 +1,31 @@
+package prime_sieve
+
+import "fmt"
+
+func generate(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
+}
+
+func filter(in <-chan int, out chan<- int, prime int) {
+	for {
+		i := <-in
+		if i%prime != 0 {
+			out <- i
+		}
+	}
+}
+
+func Sieve(n int) {
+	ch := make(chan int)
+	go generate(ch)
+	for i := 0; i < n; i++ {
+		prime := <-ch
+		fmt.Print(prime, " ")
+		ch1 := make(chan int)
+		go filter(ch, ch1, prime)
+		ch = ch1
+	}
+}
+
---
+++ prime/prime_sieve/prime_sieve_test.go
@@ -0,0 +1,8 @@
+package prime_sieve
+
+import "testing"
+
+func TestSieve(t *testing.T) {
+	Sieve(10)
+}
+
